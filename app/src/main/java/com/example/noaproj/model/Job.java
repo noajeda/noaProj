f05706ac67146070db3858273a9f10e740bd7878
Student-2024 <student-2024@ashalim.org>
2025-11-05 15:02:12 +0200

    init

---
+++ app/src/main/java/com/example/noaproj/model/Job.java
@@ -0,0 +1,115 @@
+package com.example.noaproj.model;
+
+public class Job {
+    String id;
+    String title;
+    String type;
+    String city;
+    String address;
+    String phone;
+    String age;
+    String details;
+    User user;
+
+    public Job(String address, String age, String city, String details, String id, String phone, String title, String type, User user) {
+        this.address = address;
+        this.age = age;
+        this.city = city;
+        this.details = details;
+        this.id = id;
+        this.phone = phone;
+        this.title = title;
+        this.type = type;
+        this.user = user;
+    }
+
+    public Job() {
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "Job{" +
+                "address='" + address + '\'' +
+                ", id='" + id + '\'' +
+                ", title='" + title + '\'' +
+                ", type='" + type + '\'' +
+                ", city='" + city + '\'' +
+                ", phone='" + phone + '\'' +
+                ", age='" + age + '\'' +
+                ", details='" + details + '\'' +
+                ", user=" + user +
+                '}';
+    }
+}
---
+++ app/src/main/java/com/example/noaproj/model/User.java
@@ -0,0 +1,113 @@
+package com.example.noaproj.model;
+
+public class User {
+    String id;
+    String fName;
+    String lName;
+    String phone;
+    String email;
+    String password;
+    String age;
+    String gender;
+    String city;
+
+    public User(String age, String city, String email, String fName, String gender, String id, String lName, String password, String phone) {
+        this.age = age;
+        this.city = city;
+        this.email = email;
+        this.fName = fName;
+        this.gender = gender;
+        this.id = id;
+        this.lName = lName;
+        this.password = password;
+        this.phone = phone;
+    }
+    public User() {
+    }
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "age='" + age + '\'' +
+                ", id='" + id + '\'' +
+                ", fName='" + fName + '\'' +
+                ", lName='" + lName + '\'' +
+                ", phone='" + phone + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", gender='" + gender + '\'' +
+                ", city='" + city + '\'' +
+                '}';
+    }
+}
