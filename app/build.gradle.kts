b58bf23fb3cdf57cf0e7795592e4afced24991f4
Student-2024 <student-2024@ashalim.org>
2025-12-31 15:14:56 +0200

    init

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,7 @@ dependencies {
     implementation(libs.constraintlayout)
     implementation(libs.firebase.auth)
     implementation(libs.firebase.database)
+    implementation(libs.recyclerview)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
---
+++ app/src/main/java/com/example/noaproj/userList.java
@@ -0,0 +1,45 @@
+package com.example.noaproj;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class userList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_user_list);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        RecyclerView usersList = findViewById(R.id.rv_users_list);
+        tvUserCount = findViewById(R.id.tv_user_count);
+        usersList.setLayoutManager(new LinearLayoutManager(this));
+        userAdapter = new UserAdapter(new UserAdapter.OnUserClickListener() {
+            @Override
+            public void onUserClick(User user) {
+                // Handle user click
+                Log.d(TAG, "User clicked: " + user);
+                Intent intent = new Intent(UsersListActivity.this, UserProfileActivity.class);
+                intent.putExtra("USER_UID", user.getUid());
+                startActivity(intent);
+            }
+
+            @Override
+            public void onLongUserClick(User user) {
+                // Handle long user click
+                Log.d(TAG, "User long clicked: " + user);
+            }
+        });
+        usersList.setAdapter(userAdapter);
+    }
+}
