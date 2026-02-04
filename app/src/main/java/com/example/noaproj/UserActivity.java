5ed3f24709b60b01ce7e5ebe272d848a3be4a6f3
noajeda <noajeda1@gmail.com>
2026-02-04 11:59:01 +0200

    Update UserActivity.java

--- app/src/main/java/com/example/noaproj/UserActivity.java
+++ app/src/main/java/com/example/noaproj/UserActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,20 +21,18 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class UserActivity extends AppCompatActivity implements View.OnClickListener {
    Button btnSearch, btnFilter, btnOffer, btnAnswer, btnChat, btnLogOut, btnUserList, btnJobList;
+   ImageView imgMenu;
+    private boolean isMenuOpen = false;
+
     private DatabaseService databaseService;
     private FirebaseAuth mAuth;
+
 
     private static final String TAG = "UserActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        try {
-            Log.d(TAG, "onCreate started");
-            // כל הקוד שלך כאן...
-        } catch (Exception e) {
-            Log.e(TAG, "Exception in onCreate", e);
-        }
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_user_activity);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
@@ -46,27 +45,9 @@ public class UserActivity extends AppCompatActivity implements View.OnClickListe
         initListeners();
         Log.d(TAG, "Views initialized");
 
-            mAuth = FirebaseAuth.getInstance();
-            String uid = mAuth.getCurrentUser().getUid();
-            databaseService = DatabaseService.getInstance();
-
-        databaseService.getUser(uid, new DatabaseService.DatabaseCallback<User>() {
-            public void onCompleted(User user) {
-                Log.d(TAG, "onCompleted: id:" + uid);
-                if (user.getIsAdmin()) {
-                    btnUserList.setVisibility(View.VISIBLE);
-                    btnJobList.setVisibility(View.VISIBLE);
-                }
-            }
-
-            @Override
-            public void onFailed(Exception e) {
-                Log.e(TAG, "onFailed: Failed to admin", e);
-            }
-        });
-
     }
     private void initViews(){
+        imgMenu = findViewById(R.id.imgMenu);
         btnSearch = findViewById(R.id.btnSearch);
         btnFilter = findViewById(R.id.btnFilter);
         btnOffer = findViewById(R.id.btnOffer);
@@ -77,6 +58,7 @@ public class UserActivity extends AppCompatActivity implements View.OnClickListe
         btnJobList = findViewById(R.id.btnJobList);
     }
     private void initListeners() {
+        imgMenu.setOnClickListener(this);
         btnSearch.setOnClickListener(this);
         btnFilter.setOnClickListener(this);
         btnOffer.setOnClickListener(this);
@@ -89,6 +71,28 @@ public class UserActivity extends AppCompatActivity implements View.OnClickListe
 
         @Override
     public void onClick(View v) {
+        if(v ==imgMenu && !isMenuOpen){
+            btnSearch.setVisibility(View.VISIBLE);
+            btnFilter.setVisibility(View.VISIBLE);
+            btnOffer.setVisibility(View.VISIBLE);
+            btnAnswer.setVisibility(View.VISIBLE);
+            btnChat.setVisibility(View.VISIBLE);
+            btnLogOut.setVisibility(View.VISIBLE);
+            isAdmin();
+            isMenuOpen = true;
+        }
+        else if (v == imgMenu)
+            {
+                btnSearch.setVisibility(View.GONE);
+                btnFilter.setVisibility(View.GONE);
+                btnOffer.setVisibility(View.GONE);
+                btnAnswer.setVisibility(View.GONE);
+                btnChat.setVisibility(View.GONE);
+                btnLogOut.setVisibility(View.GONE);
+                btnUserList.setVisibility(View.GONE);
+                btnJobList.setVisibility(View.GONE);
+                isMenuOpen = false;
+            }
         if(v == btnOffer){
             Intent goOffer = new Intent(this, SubmitOfferActivity.class);
             startActivity(goOffer);
@@ -117,5 +121,26 @@ public class UserActivity extends AppCompatActivity implements View.OnClickListe
         startActivity(intent);
 
         finish();
+    }
+
+    private void isAdmin(){
+        mAuth = FirebaseAuth.getInstance();
+        String uid = mAuth.getCurrentUser().getUid();
+        databaseService = DatabaseService.getInstance();
+
+        databaseService.getUser(uid, new DatabaseService.DatabaseCallback<User>() {
+            public void onCompleted(User user) {
+                Log.d(TAG, "onCompleted: id:" + uid);
+                if (user.getIsAdmin()) {
+                    btnUserList.setVisibility(View.VISIBLE);
+                    btnJobList.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void onFailed(Exception e) {
+                Log.e(TAG, "onFailed: Failed to admin", e);
+            }
+        });
     }
 }
