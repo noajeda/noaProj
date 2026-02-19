7b562d686c6ebe4347a63239aa2bbf382ac317b3
השם שלך <noajeda1@gmail.com>
2026-02-19 02:02:14 +0200

    style

--- app/src/main/java/com/example/noaproj/UserActivity.java
+++ app/src/main/java/com/example/noaproj/UserActivity.java
@@ -8,8 +8,11 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -25,14 +28,15 @@ import com.example.noaproj.model.Job;
 import com.example.noaproj.model.User;
 import com.example.noaproj.services.DatabaseService;
 import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.android.material.slider.RangeSlider;
 import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class UserActivity extends AppCompatActivity implements View.OnClickListener {
-    Button btnSearch, btnFilter, btnOffer, btnAnswer, btnChat, btnLogOut, btnUserList, btnJobList, btnMyOffersJobs;
-    ImageView imgMenu;
+    Button btnFilter, btnAnswer, btnChat, btnLogOut, btnUserList, btnJobList, btnMyOffersJobs;
+    ImageView imgMenu, imgSearchJob;
     private boolean isMenuOpen = false;
     OfferAdapter adapter;
     RecyclerView rvApproveJobs;
@@ -41,6 +45,7 @@ public class UserActivity extends AppCompatActivity implements View.OnClickListe
     private FirebaseAuth mAuth;
     ArrayList<Job> approveArraylist = new ArrayList<Job>();
 
+    EditText etSearchJob;
     private static final String TAG = "UserActivity";
 
     @Override
@@ -66,9 +71,7 @@ public class UserActivity extends AppCompatActivity implements View.OnClickListe
 
     private void initViews(){
         imgMenu = findViewById(R.id.imgMenu);
-        btnSearch = findViewById(R.id.btnSearch);
         btnFilter = findViewById(R.id.btnFilter);
-        btnOffer = findViewById(R.id.btnOffer);
         btnAnswer = findViewById(R.id.btnAnswer);
         btnChat = findViewById(R.id.btnChat);
         btnLogOut = findViewById(R.id.btnLogOut);
@@ -78,7 +81,8 @@ public class UserActivity extends AppCompatActivity implements View.OnClickListe
         flMenu = findViewById(R.id.flMenu);
 
         btnMyOffersJobs = findViewById(R.id.btnMyOffersJobs);
-
+        etSearchJob = findViewById(R.id.etSearchJob);
+        imgSearchJob = findViewById(R.id.imgSearchJob);
 
 
         rvApproveJobs.setLayoutManager(new LinearLayoutManager(this));
@@ -101,14 +105,13 @@ public class UserActivity extends AppCompatActivity implements View.OnClickListe
     }
     private void initListeners() {
         imgMenu.setOnClickListener(this);
-        btnSearch.setOnClickListener(this);
         btnFilter.setOnClickListener(this);
-        btnOffer.setOnClickListener(this);
         btnAnswer.setOnClickListener(this);
         btnChat.setOnClickListener(this);
         btnLogOut.setOnClickListener(this);
         btnUserList.setOnClickListener(this);
         btnJobList.setOnClickListener(this);
+        imgSearchJob.setOnClickListener(this);
 
         btnMyOffersJobs.setOnClickListener(this);
     }
@@ -122,36 +125,19 @@ public class UserActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         if(v ==imgMenu && !isMenuOpen){
-            btnSearch.setVisibility(View.VISIBLE);
-            btnFilter.setVisibility(View.VISIBLE);
-            btnOffer.setVisibility(View.VISIBLE);
-            btnAnswer.setVisibility(View.VISIBLE);
-            btnChat.setVisibility(View.VISIBLE);
-            btnLogOut.setVisibility(View.VISIBLE);
-            btnMyOffersJobs.setVisibility(View.VISIBLE);
+            flMenu.setVisibility(View.VISIBLE);
             flMenu.setBackgroundColor(Color.parseColor("#CCFFFFFF"));
             isAdmin();
             isMenuOpen = true;
         }
         else if (v == imgMenu)
         {
-            btnSearch.setVisibility(View.GONE);
-            btnFilter.setVisibility(View.GONE);
-            btnOffer.setVisibility(View.GONE);
-            btnAnswer.setVisibility(View.GONE);
-            btnChat.setVisibility(View.GONE);
-            btnLogOut.setVisibility(View.GONE);
+            flMenu.setVisibility(View.GONE);
             btnUserList.setVisibility(View.GONE);
             btnJobList.setVisibility(View.GONE);
-            btnMyOffersJobs.setVisibility(View.GONE);
 
             flMenu.setBackground(new ColorDrawable(Color.TRANSPARENT));
             isMenuOpen = false;
-        }
-        if(v == btnOffer){
-            Intent goOffer = new Intent(this, SubmitOfferActivity.class);
-            startActivity(goOffer);
-
         }
         if(v == btnUserList){
             Intent goUserList = new Intent(this, userList.class);
@@ -173,17 +159,134 @@ public class UserActivity extends AppCompatActivity implements View.OnClickListe
             BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(UserActivity.this);
             bottomSheetDialog.setContentView(R.layout.botton_sheet_filter);
             bottomSheetDialog.show();
-            Button btnApplyFilter = bottomSheetDialog.findViewById(R.id.btnApplyFilter);
-            if (btnApplyFilter != null) {
-                btnApplyFilter.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        bottomSheetDialog.dismiss();
-                    }
-                });
-            }
+            showFilter(bottomSheetDialog);
+        }
+        if(v == imgSearchJob){
+            searchJob();
         }
 
+    }
+
+    private void searchJob() {
+        String findJob = etSearchJob.getText().toString();   // העבודה המבוקשת
+        ArrayList<Job> searchJobList = new ArrayList<>();
+        for(int i =0; i < approveArraylist.size(); i++){
+            if(approveArraylist.get(i).getCompany().contains(findJob))
+                searchJobList.add(approveArraylist.get(i));
+        }
+        adapter.setJobList(searchJobList);  // רשימה של כל העבודות של חברה מסוימת
+    }
+
+    private void showFilter(BottomSheetDialog bottomSheetDialog) {  // filter bottom sheet
+
+
+
+        RangeSlider sliderAge = bottomSheetDialog.findViewById(R.id.sliderAge);
+        List<Float> initialValues = new ArrayList<>();
+        initialValues.add(16f); // יד שמאלית
+        initialValues.add(60f); // יד ימנית
+        sliderAge.setValues(initialValues);
+
+
+        // cities
+        LinearLayout layoutCities = bottomSheetDialog.findViewById(R.id.layoutCities);
+        ArrayList<CheckBox> cbCites = new ArrayList<>();  // רשימת ערים עבור בדיקה
+        String[] cities = getResources().getStringArray(R.array.arrCity);
+        for(int i = 0; i< cities.length; i++){
+            String city = cities[i];
+            CheckBox cb = new CheckBox(this);
+            cb.setText(city);
+            layoutCities.addView(cb);   // הוספה לתצוגה
+            cbCites.add(cb);    // שמירה ברשימה עבור בדיקה
+        }
+        // types
+        LinearLayout layoutTypes = bottomSheetDialog.findViewById(R.id.layoutTypes);
+        ArrayList<CheckBox> cbTypes = new ArrayList<>();  // רשימת סוגים עבור בדיקה
+        String[] types = getResources().getStringArray(R.array.arrType);
+        for(int i = 0; i< types.length; i++){
+            String type = types[i];
+            CheckBox cb = new CheckBox(this);
+            cb.setText(type);
+            layoutTypes.addView(cb);  // הוספה לתצוגה
+            cbTypes.add(cb);    // שמירה ברשימה עבור בדיקה
+
+        }
+        // titles
+        LinearLayout layoutTitles = bottomSheetDialog.findViewById(R.id.layoutTitle);
+        ArrayList<CheckBox> cbTitles = new ArrayList<>(); // רשימת תפקידים עבור בדיקה
+        String[] titles = getResources().getStringArray(R.array.arrTitle);
+        for(int i = 0; i< titles.length; i++){
+            String title = titles[i];
+            CheckBox cb = new CheckBox(this);
+            cb.setText(title);
+            layoutTitles.addView(cb);
+            cbTitles.add(cb);    // שמירה ברשימה עבור בדיקה
+        }
+        // btnApply
+        Button btnApplyFilter = bottomSheetDialog.findViewById(R.id.btnApplyFilter);
+        if (btnApplyFilter != null) {
+            btnApplyFilter.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    ArrayList<String> selectedCities = new ArrayList<>(); // הערים שנבחרו
+                    for (int i = 0; i < cities.length; i++) {
+                        CheckBox cb = cbCites.get(i);
+                        if (cb.isChecked()) {
+                            selectedCities.add(cb.getText().toString());  // שמירה
+                        }
+                    }
+                    ArrayList<String> selectedTypes = new ArrayList<>(); // הסוגים שנבחרו
+                    for (int i = 0; i < types.length; i++) {
+                        CheckBox cb = cbTypes.get(i);
+                        if (cb.isChecked()) {
+                            selectedTypes.add(cb.getText().toString());  // שמירה
+                        }
+                    }
+                    ArrayList<String> selectedTitles = new ArrayList<>(); // התפקידים שנבחרו
+                    for (int i = 0; i < titles.length; i++) {
+                        CheckBox cb = cbTitles.get(i);
+                        if (cb.isChecked()) {
+                            selectedTitles.add(cb.getText().toString());  // שמירה
+                        }
+                    }
+
+
+                    // טווח גילים
+                    List<Float> values = sliderAge.getValues();
+                    int minAge = Math.round(values.get(0)); // היד השמאלית
+                    int maxAge = Math.round(values.get(1)); // היד הימנית
+
+                    filterJobs(selectedCities, selectedTypes, selectedTitles, minAge, maxAge);
+                    // סגירה
+                    bottomSheetDialog.dismiss();
+                }
+            });
+        }
+    }
+    private void filterJobs(ArrayList<String> selectedCities, ArrayList<String> selectedTypes, ArrayList<String> selectedTitles, int minAge, int maxAge){
+        ArrayList<Job> filteredJobs = new ArrayList<>();
+        for(int i=0; i<approveArraylist.size(); i++) {
+            Job job = approveArraylist.get(i);
+            boolean match = true;
+            if (!selectedCities.isEmpty()) {
+                match = match && selectedCities.contains(job.getCity());
+            }
+
+            if (!selectedTypes.isEmpty()) {
+                match = match && selectedTypes.contains(job.getType());
+            }
+
+            if (!selectedTitles.isEmpty()) {
+                match = match && selectedTitles.contains(job.getTitle());
+            }
+         //   if(job.getAge() < String.valueOf(minAge) || job.getAge() > maxAge)
+             //   match= false;
+
+            if (match) {     // בדיקה האם ישנה עבודה התואמת לדרישות
+                filteredJobs.add(job);
+            }
+        }
+        adapter.setJobList(filteredJobs);
     }
     private void logOut() {
         SharedPreferences prefs = getSharedPreferences("MyAppPrefs", MODE_PRIVATE);
@@ -196,7 +299,7 @@ public class UserActivity extends AppCompatActivity implements View.OnClickListe
         finish();
     }
 
-    private void isAdmin(){
+    private void isAdmin(){       // admin
         mAuth = FirebaseAuth.getInstance();
         String uid = mAuth.getCurrentUser().getUid();
 
@@ -205,8 +308,8 @@ public class UserActivity extends AppCompatActivity implements View.OnClickListe
                 Log.d(TAG, "onCompleted: id:" + uid);
                 if(user!=null) {
                     if (user.getIsAdmin()) {
-                        btnUserList.setVisibility(View.VISIBLE);
-                        btnJobList.setVisibility(View.VISIBLE);
+                        btnUserList.setVisibility(View.VISIBLE);     // show btnUserList
+                        btnJobList.setVisibility(View.VISIBLE);      // show btnJobList
                     }
                 }
             }
