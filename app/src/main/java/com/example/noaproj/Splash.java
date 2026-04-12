85d5be0f018fb8141860946fceda37a119f05a4a
השם שלך <noajeda1@gmail.com>
2026-04-12 08:09:01 +0300

    alarm

--- app/src/main/java/com/example/noaproj/Splash.java
+++ app/src/main/java/com/example/noaproj/Splash.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
